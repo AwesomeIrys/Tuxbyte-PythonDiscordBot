8e6f2126338a6935f651651156b75c202b1f0263
Iris <165673695+AwesomeIrys@users.noreply.github.com>
2024-11-13 00:40:12 -0500

    Update tuxbyte.py

--- tuxbyte.py
+++ tuxbyte.py
@@ -1,9 +1,22 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+import mysql.connector
+from db import get_db_cursor, close_db_connection  # Import the function to get the connection and cursor
+# Import the ping command from ping.py
+from ping import ping
 
-# Replace 'TOKEN_HERE' with your bot's token
-TOKEN = 'TOKEN_HERE'
-PREFIX = '!'
+# Load environment variables from .env file
+load_dotenv()
+
+# Get the database connection and cursor
+db, cursor = get_db_cursor()
+
+# Retrieve the bot's token and prefix from environment variables
+TOKEN = os.getenv("BOT_TOKEN")
+PREFIX = os.getenv("BOT_PREFIX")
+
 bot = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
 
 # Set intents to access members and messages
@@ -24,6 +37,10 @@ async def on_ready():
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandNotFound):
         await ctx.send(f"{ctx.author.mention}, command not found. Please check the command and try again!")
+
+@bot.event
+async def on_close():
+    close_db_connection(db, cursor)
 
 # Run the bot
 bot.run(TOKEN)
